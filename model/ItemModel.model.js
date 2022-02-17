8913634401b86ab9a5bdcd9bbe4f0e743883bab4
Afzal Ali <afzalali1999@gmail.com>
2022-02-17 15:10:53 -0500

    fix routes

--- model/ItemModel.model.js
+++ model/ItemModel.model.js
@@ -28,13 +28,44 @@ class ItemModelModel {
         try {
             const mysql = connection? connection : pool;
 
-            const stmt = 'SELECT FROM item_model WHERE model_name = ?';
+            const stmt = 'SELECT * FROM item_model WHERE model_name = ?';
             const results = await mysql.query(stmt, [ searchQuery ]);
 
             return results[0];
         } catch (error) {
             throw new Error(error);
         }
+    }
+
+    // --
+
+    static async create(item, connection=null) {
+        try {
+            const mysql = connection? connection : pool;
+
+            const stmt = 'INSERT INTO item SET ?';
+            await mysql.query(stmt, item);
+        } catch (error) { throw new Error(error); }
+    }
+
+    static async update(id, updatedItem, connection=null) {
+        try {
+            const mysql = connection? connection : pool;
+
+            const stmt = 'UPDATE item SET ? WHERE item_id = ?';
+            await mysql.query(stmt, [ updatedItem, id ]);
+
+            return;
+        } catch (error) { throw new Error(error); }
+    }
+
+    static async delete(id, connection=null) {
+        try {
+            const mysql = connection? connection : pool;
+
+            const stmt = 'DELETE FROM item WHERE item_id = ?';
+            await mysql.query(stmt, [ id ]);
+        } catch (error) { throw new Error(error); }
     }
 }
 
--- routes/itemModelRoutes.js
+++ routes/itemModelRoutes.js
@@ -2,11 +2,10 @@
 const router = require('express').Router();
 const ItemModel = require('../model/ItemModel.model');
 
-// get all inventory item models
+// get all item models
 router.get('/', async (req, res, next) => {
     try {
-        const searchQuery = req.query;
-        console.log('SEARCH ' + JSON.stringify(searchQuery));
+        const searchQuery = req.query.search;
         let itemModels = null;
 
         if (searchQuery) {
@@ -19,12 +18,41 @@ router.get('/', async (req, res, next) => {
     } catch (error) { next(error); }
 });
 
-// get an inventory item by id
+// get an item model by id
 router.get('/:id', async (req, res, next) => {
     try {
-        const item = await Item.get(req.params.id);
+        const item = await ItemModel.get(req.params.id);
 
         res.send({ success: true, item });
+    } catch (error) { next(error); }
+});
+
+// --
+
+// create an item
+router.post('/', async (req, res, next) => {
+    try {
+        await ItemModel.create(req.body)
+
+        res.send({ success:true });
+    } catch (error) { next(error); }
+});
+
+// update an item
+router.get('/:id', async (req, res, next) => {
+    try {
+        await ItemModel.update(req.params.id, req.body);
+
+        res.send({ success: true });
+    } catch (error) { next(error); }
+});
+
+// delete an item
+router.delete('/:id', async (req, res, next) => {
+    try {
+        await ItemModel.delete(req.params.id);
+
+        res.send({ success: true });
     } catch (error) { next(error); }
 });
 
